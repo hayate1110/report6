e1ec44f1d65e9064192ae55eccb25ba2ecd31e01
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2022-12-18 10:20:08 +0900

    create and fixed class Player and Wolf

--- app/src/main/java/jp/ac/uryukyu/ie/e225719/Player.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225719/Player.java
@@ -1,7 +1,9 @@
 package jp.ac.uryukyu.ie.e225719;
 import java.util.Map;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Player {
@@ -14,26 +16,33 @@ public class Player {
         this.isDead = false;
     }
 
-    public void vote(ArrayList<Player> players) {
-        System.out.println(players);
-        System.out.println("処刑したい人の名前を入力してください。");
-        for (int i=0;i<players.size();i++) {
-            System.out.println(players.get(i).getName());
-        }
-        Scanner scanner = new Scanner(System.in);
-
-        loop: while (true) {
-            String line = scanner.next();
-            System.out.println(line);
-            for (Player p: players) {
-                if (p.getName().equals(line) && !p.isDead()) {
-                    int v = executionList.getOrDefault(p,0);
-                    v += 1;
-                    executionList.put(p,v);
-                    break loop;
-                }
+    public void vote(ArrayList<Player> players, Scanner sc) {
+        for(int i=0;i<players.size();i++) {
+            Player p = players.get(i);
+            if(!p.isDead) {
+                System.out.println(i + ":" + p.getName());
             }
-            System.out.println("そのプレイヤーは死亡しているか存在しません。");
+        }
+
+        System.out.println();
+        while(true) {
+            try {
+                System.out.print("処刑したいプレイヤーの番号を入力してください:");
+                int num = sc.nextInt();
+                Player p = players.get(num);
+                
+                if(!p.isDead) {
+                    int value = executionList.getOrDefault(p, 0);
+                    value += 1;
+                    executionList.put(p, value);
+                    break;
+                }
+            } catch(InputMismatchException e) {
+                sc.nextLine();
+                System.out.println("整数を入力してください。");
+            } catch(IndexOutOfBoundsException e) {
+                System.out.println("その番号のプレイヤーは存在しません。");
+            }
         }
     }
 
